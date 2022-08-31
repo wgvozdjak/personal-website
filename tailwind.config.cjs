68409f917bd6105bb1ad5a63e98d1edd3b8b62d9
William Gvozdjak <w.gvoz@outlook.com>
2022-08-30 17:19:43 -0700

    Layout basis of homepage design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,10 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			'josefin': ['"Josefin Sans"', 'sans-serif']
+			'josefin': ['"Josefin Sans"', 'sans-serif'],
+			'ibm-plex-sans': ['"IBM Plex Sans"', 'sans-serif'],
+			'inter': ['"Inter"', 'sans-serif'],
+			'montserrat': ['"Montserrat"', 'sans-serif'],
 		},
 		extend: {
 		},
