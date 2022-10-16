476bff12a17343e6a7907e2191cd468440aaebd4
William Gvozdjak <w.gvoz@outlook.com>
2022-10-16 12:39:20 -0700

    Run Prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			'josefin': ['"Josefin Sans"', 'sans-serif'],
-			'ibm-plex-sans': ['"IBM Plex Sans"', 'sans-serif'],
-			'inter': ['"Inter"', 'sans-serif'],
-			'montserrat': ['"Montserrat"', 'sans-serif'],
-		},
-		extend: {
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-	darkMode: 'class',
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      josefin: ['"Josefin Sans"', "sans-serif"],
+      "ibm-plex-sans": ['"IBM Plex Sans"', "sans-serif"],
+      inter: ['"Inter"', "sans-serif"],
+      montserrat: ['"Montserrat"', "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
+};
